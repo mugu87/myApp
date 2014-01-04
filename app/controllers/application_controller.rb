1c74167e0bc57e80e17306b0befe61842834a47f
landseaco <ryan@landandseaco.com>
2014-01-04 14:59:44 -0600

    Added Fund to index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :get_event
   def after_sign_in_path_for(resource_or_scope)
   	if request.env['omniauth.origin']
   		request.env['omniauth.origin']
   	end
   	user_path(current_user)
+  end
+
+
+  private
+  def get_event
+  	@crescent_event = Event.find(1)
   end
 end
