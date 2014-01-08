7e65a1639e88694c1d8edc23160abd6604c5341b
ntran2 <ntran2@uno.edu>
2014-01-08 16:13:31 -0600

    rackspace deploy

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -2,21 +2,11 @@
 #
 # Table name: stats
 #
-#  id                      :integer          not null, primary key
-#  today_active_calories   :integer
-#  user_id                 :integer
-#  step_count_walking      :integer
-#  step_count_running      :integer
-#  seconds_walking         :integer
-#  seconds_running         :integer
-#  kilometers_walking      :float
-#  kilometers_running      :float
-#  calories_burned_walking :integer
-#  calories_burned_running :integer
-#  data_source             :string(255)
-#  stat_date               :date
-#  created_at              :datetime
-#  updated_at              :datetime
+#  id                    :integer          not null, primary key
+#  today_active_calories :integer
+#  user_id               :integer
+#  created_at            :datetime
+#  updated_at            :datetime
 #
 
 class Stat < ActiveRecord::Base
